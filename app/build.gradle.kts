dd47c2b676d3218685d837386785591be1cb710d
kiryl.kvit <Kiryl.Kvit@nxtec-corp.com>
2024-01-07 18:58:32 +0100

    Versino update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.honorida"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0-alpha.2"
+        versionCode = 2
+        versionName = "1.0.0-alpha.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
