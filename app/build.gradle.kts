8c825483ce0d0c71ca379e3a8a0cae20e2e687d5
kiryl.kvit <Kiryl.Kvit@nxtec-corp.com>
2024-01-09 12:04:36 +0100

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 3
-        versionName = "1.0.0-alpha.1"
+        versionName = "1.0.0-alpha.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
